7baa04ae3c5825f6a8bd6571e0d1b38bbf7ec6d9
mhezzet <mhezzet@gmail.com>
2018-11-15 14:11:06 +0200

    test 5

--- middleware/usersAuth/googleAccessTokenValidator.js
+++ middleware/usersAuth/googleAccessTokenValidator.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 module.exports = async function(req, res, next) {
   const token = req.body.accessToken;
+  console.log(req.body)
   if (!token) return res.status(401).send('Access denied , no token provided');
   try {
     const response = await axios.get(
